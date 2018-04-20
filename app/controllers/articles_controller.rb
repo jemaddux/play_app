7fd8d44d32a739eb4fdd548fe6350488e41468d5
John Maddux <jemaddux@gmail.com>
2018-04-19 20:22:27 -0400

    Fixed tests so they actually work. Refactored part of the articles_controller into a generic controller for fun and because code climate thinks there is too much duplication.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,31 +1,12 @@
 class ArticlesController < ApplicationController
+  include GenericController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
   decorates_assigned :articles, :article
-
-  # GET /articles
-  # GET /articles.json
-  def index
-    @articles = Article.all
-  end
-
-  # GET /articles/1
-  # GET /articles/1.json
-  def show
-  end
-
-  # GET /articles/new
-  def new
-    @article = Article.new
-  end
-
-  # GET /articles/1/edit
-  def edit
-  end
 
   # POST /articles
   # POST /articles.json
   def create
-    @article = Article.new(article_params)
+    @article = Article.new(controller_params)
 
     respond_to do |format|
       if @article.save
@@ -33,20 +14,6 @@ class ArticlesController < ApplicationController
         format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /articles/1
-  # PATCH/PUT /articles/1.json
-  def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit }
         format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
@@ -66,10 +33,5 @@ class ArticlesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def article_params
-      params.require(:article).permit(:title, :body)
     end
 end
