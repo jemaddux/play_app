e01f19dc66457c3bd32b2a1f59e268620a0cef12
John Maddux <jemaddux@gmail.com>
2018-05-06 20:09:17 -0400

    Testing

---
+++ features/step_definitions/test_steps.rb
@@ -0,0 +1,7 @@
+When("I go to the homepage") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see the welcome message") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
